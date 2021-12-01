21b09281363c75b56363e3899aefd3744eb5d80e
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2021-12-01 14:23:56 -0800

    update

--- task1.py
+++ task1.py
@@ -1,10 +1,14 @@
 #! python3
-
+"""
 txt = input("enter name")
 
 txt2 = input("enter email address")
 
 x = txt.strip()
 y = txt2.strip()
+"""
+
+x = "Joe Lunchbox"
+y = "joe@koolsandwiches.org"
 
 print("Your name is", x, "and your email is", y)
