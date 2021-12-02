c9037a30c0cfb130048e0552f4725ffbc2d483c0
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2021-12-02 14:04:10 -0800

    update

--- task4.py
+++ task4.py
@@ -1,18 +1,21 @@
 #! python3
-
 import math
-
+#### Task 4
+"""
+Find the hypotenuse\
+Your program will ask the user to enter in the 2 short sides of a right triangle.\
+You will calculate the length of the hypotenuse and display the result.\
+You will need to use the *math* module to use the command that finds the square root.\
+(2 points)
+"""
 print("Enter value of one short side")
 a = input()
 print("Enter value of another short side")
 b = input()
 
-e = a*2 + b*2
+e = float(a) * float(a) + float(b) * float(b)
 
-number = int(input(e))
-sqrt = number ** 0.5
-
-print(sqrt)
+print (math.sqrt(e))
 
 # Find the hypotenuse
 # Your program will ask the user to enter in the 2 short sides of a right triangle.
